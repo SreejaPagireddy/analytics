80165ef6b85f518028829e08e362752b5cdd5199
Sreeja Pagireddy <spagi@ucr.edu>
2023-08-27 23:39:00 -0700

    internship page

--- src/app/page.js
+++ src/app/page.js
@@ -2,112 +2,415 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
+  
+    <div className='h-[1220px] bg-slate-200  text-black text-1xl  '>
+      <p className='relative top-5 left-6 font-bold'>
+        Summer referral competition
+      </p>
+      <button className='border border-slate-300 rounded-lg bg-slate-100 text-xs absolute right-9 top-4 px-4 py-2'>
+        1h   24h  30d  60d
+      </button>
+
+    <div className="  flex  justify-center ">
+    <div className="bg-slate-100 w-[680px] static h-[220px] rounded-lg shadow mt-10 ">
+      <p className='mt-5 ml-5 text-xl font-bold'>
+        100,000
+      </p>
+      <p className='ml-5 text-xs'>
+        Participants
+      </p>
+      <div className='h-screen ml-3 flex items-stretch translate-y-[70px]'>
+      <div class="w-2 h-5 rounded-lg relative -bottom-10  bg-[#fcd34d]   ">
+      </div>
+      <div class="w-2 h-7 rounded-lg relative -bottom-10 right-3 -mt-2 bg-[#fcd34d] ml-5 ">
+      </div>
+
+      <div class="w-2 h-5 rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3 ">
+      </div>
+
+      <div class="w-2 h-6 rounded-lg relative -bottom-10 right-3 -mt-1 bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-9 rounded-lg relative -bottom-10 right-3 -mt-3 bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-12 rounded-lg relative -bottom-10 right-3 -mt-6 bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-9 rounded-lg relative -bottom-10 right-3 -mt-3 bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-11 rounded-lg relative -bottom-10 right-3 -mt-5 bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-10 rounded-lg relative -bottom-10 right-3 -mt-4 bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-8 rounded-lg relative -bottom-10 right-3 -mt-2 bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-7 rounded-lg relative -bottom-10 right-3 -mt-1 bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-5 rounded-lg  relative -bottom-11 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-6 rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-7 -translate-y-[5px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-8 -translate-y-[9px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[37px] -translate-y-[14px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[27px] -translate-y-[5px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[35px] -translate-y-[14px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[60px] -translate-y-[39px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[45px] -translate-y-[24px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[65px] -translate-y-[44px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[95px] -translate-y-[72px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[38px] -translate-y-[16px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[67px] -translate-y-[44px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[98px] -translate-y-[77px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[65px] -translate-y-[44px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[85px] -translate-y-[65px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[92px] -translate-y-[74px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[163px] -translate-y-[144px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[176px] -translate-y-[157px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[189px] -translate-y-[169px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[125px] -translate-y-[104px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[95px] -translate-y-[74px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <div class="w-2 h-[55px] -translate-y-[34px] rounded-lg relative -bottom-10 right-3  bg-[#fcd34d] ml-3">
+      </div>
+
+      <button className='border border-slate-300 bg-slate-100 text-xs absolute right-2 top-4 px-1 py-2 translate-x-[40px] -translate-y-[100px]'>
+        <p>
+        10,000 signups
+          </p>
+          <p className='text-[9px] text-gray-500'>
+            Current week
+          </p>
+      </button>
+
+      </div>
+    </div>
+
+
+    <div className="bg-slate-100 absolute w-[680px] h-[240px] rounded-lg shadow translate-y-[290px]">
+      <p className='text-xl font-bold mt-4 ml-4'>
+        ZooTools AI-powered insights
+      </p>
+      <p className='text-xs ml-4'>
+          Making analytics simple and actionable
+      </p>
+      <button className='border border-slate-300 text-xs absolute right-2 top-4 px-1 py-2'>
+        Ask question
+      </button>
+      <button className='border py-2 px-4 border-slate-300 mr-1 absolute mt-4 ml-4 text-xs'>
+      Signups are slowing down, -.5% now than last week
+      </button>
+      <button className='border  absolute right-4 mt-4  py-2  text-[12px]'>
+       Form competion rate is good (20% conversation rate)
+      </button>
+
+      <button className='border bg-gray-300 py-2 px-2 break-after-column border-slate-300 absolute translate-y-3/4 mt-5 ml-4 text-[8px]'>
+      <p>
+      Tip: Promote more your product online, and make sure to drive as much traffic as
+      </p>
+      <p className='text-left'>
+        possible to the signup form. Here is a article on user acquisiton
+      </p>
+      </button>
+
+      <button className='border  bg-gray-300 absolute right-3 mt-5 translate-y-3/4 px-1 py-2  text-[8px]'>
+      <p>
+      Tip: You could increase conversion rates by asking fewer questions, making sure
+      </p>
+      <p className='text-left'>
+        you are bringing qualified leads to you forms.
+      </p>
+      </button>
+
+      <button className='border  bg-grey-300 py-2 px-9 border-slate-300 absolute  ml-4 text-xs bottom-9'>
+      80% of your signups are referrals, congrats!
+      </button>
+      <button className='border  bg-gray-300 absolute bottom-4 right-2 -mb-3  px-1 py-1  text-[8px]'>
+      <p>
+      Have you considred created email flows to remind your subscribers to invite their
+      </p>
+      <p className='text-left'>
+      friends and celebrate referrals milestones? Read this quide on how to do it
+      </p>
+      </button>
+      <button className='border  bg-gray-300 py-1 px-2 border-slate-300 absolute translate-y-3/4  ml-4 text-[8px] bottom-7'>
+      <p>
+      You are doing an outstanding job to motivating users to invite their friends. Make
+      </p>
+      <p className='text-left'>
+      sure to keep engaging with them with email marketing campaigns.
+      </p>
+      </button>
+      <button className='border   absolute  mr-1 right-2 bottom-4 px-14  py-1 mb-6 text-xs'>
+        <p className='text-left  '>
+        💡 Growth Tips for you to try next
+          </p>
+      </button>
+      
+     
+    </div>
+
+    <div className="bg-slate-100 absolute w-[330px] h-[265px] rounded-lg shadow translate-y-[550px] -translate-x-[170px]">
+    
+      <p className='text-s mt-3 ml-3'>
+        User leaderboard
+      </p>
+      
+      <div>
+      <p className='text-xs mt-5 ml-3'>
+        Email
+        <p className='text-xs text-center -mt-5 ml-10'>
+          Friends Invited
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <p className='text-xs text-right -mt-4 mr-3'>
+          Country
+        </p>
+      </p>
+
+      <p className='text-xs mt-4 ml-3'>
+        paula.suarze@con...es 
+        <p className='text-xs text-center -mt-5 ml-10'>
+          6,500
+        </p>
+        <p className='text-xs text-right -mt-4 mr-3'>
+          USA
+        </p>
+      </p>
+
+      <p className='text-xs mt-4 ml-3'>
+        laura.kenet..3@yahoo.com
+        <p className='text-xs text-center -mt-5 ml-10'>
+          2,300
+        </p>
+        <p className='text-xs text-right -mt-4 mr-3'>
+          Monaco
+        </p>
+      </p>
+
+      <p className='text-xs mt-4 ml-3'>
+        aaron.michael@arg.org
+        <p className='text-xs text-center -mt-5 ml-10'>
+          1,200
+        </p>
+        <p className='text-xs text-right -mt-4 mr-3'>
+          Prague
+        </p>
+      </p>
+      <p className='text-xs mt-4 ml-3'>
+        jeremy.runner@aol.org
+        <p className='text-xs text-center -mt-5 ml-10'>
+          900
+        </p>
+        <p className='text-xs text-right -mt-4 mr-3'>
+          China
+        </p>
+      </p>
+      <button className='border border-slate-300 py-1 px-1 text-xs mt-6 ml-3 bg-gray-300'>
+        See leaderboard
+      </button>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    <div className="bg-slate-100 absolute w-[330px] h-[265px] rounded-lg shadow translate-y-[550px] translate-x-[180px]">
+    <p className='text-s mt-3 ml-3'>
+        Traffic
+        <button className='text-xs absolute top-5 right-12 -translate-x-2/4 border  bg-gray-300 border-slate-300 px-1'>
+          Source
+        </button>
+        <p className='text-xs text-right -mt-4 mr-5'>
+          City
+        </p>
+      </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <p className='text-xs mt-3 ml-5'>
+        Google
+        <p className='text-xs text-right -mt-4 mr-3'>
+          30,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+        Twitter
+        <p className='text-xs text-right -mt-4 mr-3'>
+          20,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+        Facebook
+        <p className='text-xs text-right -mt-4 mr-3'>
+          5,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+        Linkedln
+        <p className='text-xs text-right -mt-4 mr-3'>
+          3,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+        YouTube
+        <p className='text-xs text-right -mt-4 mr-3'>
+          2,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+        Other
+      </p>
+      <button className='border border-slate-300 py-1 px-1 text-xs mt-2 ml-3 bg-gray-300'>
+        See traffic sources
+      </button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+    </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className="bg-slate-100 absolute w-[330px] h-[265px] rounded-lg shadow translate-y-[850px] -translate-x-[170px]">
+    <p className='text-s mt-3 ml-3'>
+        Signup Location
+        <button className='text-xs absolute top-5 right-12 -translate-x-2/4 border  bg-gray-300 border-slate-300 px-1'>
+          Country
+        </button>
+        <p className='text-xs text-right -mt-4 mr-5'>
+          City
+        </p>
+      </p>
+    
+      <p className='text-[13px] mt-3 ml-5'>
+      🇺🇲 United States
+        <p className='text-xs text-right -mt-4 mr-3'>
+          30,000
+        </p>
+      </p>
+      <p className='text-[13px] mt-3 ml-5'>
+      🇩🇪 Germany
+        <p className='text-xs text-right -mt-4 mr-3'>
+          20,000
+        </p>
+      </p>
+      <p className='text-[13px] mt-3 ml-5'>
+      🇳🇱 Netherlands
+        <p className='text-xs text-right -mt-4 mr-3'>
+          5,000
+        </p>
+      </p>
+      <p className='text-[13px] mt-3 ml-5'>
+      🇮🇳 India
+        <p className='text-xs text-right -mt-4 mr-3'>
+          3,000
+        </p>
+      </p>
+      <p className='text-[13px] mt-3 ml-5'>
+      🇯🇵 Japan
+        <p className='text-xs text-right -mt-4 mr-3'>
+          2,000
+        </p>
+      </p>
+      <p className='text-[13px] mt-3 ml-5'>
+      🌎 Other
+      </p>
+      <button className='border border-slate-300 py-1 px-1 text-xs mt-2 ml-3 bg-gray-300'>
+        See all countires
+      </button>
+    </div>
+
+    <div className="bg-slate-100 absolute w-[330px] h-[265px] rounded-lg shadow translate-y-[850px] translate-x-[180px]">
+    <p className='text-s mt-3 ml-3'>
+        Behaviour
+
+        <button className='text-xs absolute top-5 right-12 -translate-x-2/4 border  bg-gray-300 border-slate-300 px-1'>
+          Country
+        </button>
+
+        <p className='text-xs text-right -mt-4 mr-5'>
+          Decides
+        </p>
+      </p>
+
+      <p className='text-xs mt-3 ml-5'>
+        United States
+        <p className='text-xs text-right -mt-4 mr-3'>
+          30,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+      Germany
+        <p className='text-xs text-right -mt-4 mr-3'>
+          20,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+      Netherlands
+        <p className='text-xs text-right -mt-4 mr-3'>
+          5,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+        India
+        <p className='text-xs text-right -mt-4 mr-3'>
+          3,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+        Japan
+        <p className='text-xs text-right -mt-4 mr-3'>
+          2,000
+        </p>
+      </p>
+      <p className='text-xs mt-3 ml-5'>
+        Other
+      </p>
+      <button className='border border-slate-300 py-1 px-1 text-xs mt-2 ml-3 bg-gray-300'>
+        See all countires
+      </button>
+    </div>
+
+    </div>
+    </div>
+  );
 }
